a4cf0f2cd434a08fd56caa6be2e019863ebfbf69
Peter Bjerg Mogensen <pbm@impact.dk>
2020-04-16 11:49:45 +0200

    UDV-1099: Now keeps the number in between pages
    - cleans up the array after a tab is closed

--- background.js
+++ background.js
@@ -1,11 +1,26 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendReponse) {
-    chrome.browserAction.getBadgeText({tabId: sender.tab.id}, function (result) {
-        var count = parseInt(result);
-        if (isNaN(count)) {
-            count = request.add;
-        } else {
-            count += request.add;
-        }
-        chrome.browserAction.setBadgeText({text: '' + count, tabId: sender.tab.id});
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let tabId = sender.tab.id;
+    chrome.storage.local.get(['countArray'], function (countTracker) {
+        let count = updateCount(countTracker, tabId, request.add);
+        let countText = count > 0 ? count : '';
+        chrome.browserAction.setBadgeText({text: '' + countText, tabId: tabId});
     });
 });
+chrome.runtime.onInstalled.addListener(function (event ) {
+    chrome.storage.local.set({countArray: []});
+});
+chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
+    chrome.storage.local.get(['countArray'], function (countTracker) {
+        delete countTracker.countArray[tabId];
+        chrome.storage.local.set(countTracker);
+    });
+});
+
+function updateCount(countTracker, tabId, add) {
+    let count = countTracker.countArray[tabId];
+    count = count === undefined ? 0 : count;
+    count += add;
+    countTracker.countArray[tabId] = count;
+    chrome.storage.local.set(countTracker);
+    return count;
+}
--- content.js
+++ content.js
@@ -4,3 +4,8 @@ window.addEventListener('popstate', function (event)
     history.pushState(null, document.title, location.href);
     chrome.runtime.sendMessage({add: 1});
 });
+
+// Initialize the counter and refreshes the show number on page shifts
+window.addEventListener('load', function (event) {
+    chrome.runtime.sendMessage({add: 0}); 
+});
